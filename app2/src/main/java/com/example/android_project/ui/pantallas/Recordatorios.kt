ed6a555777f6e9056ea2bfef3ad7790e70b21600
lorena-14 <melissa14.049@gmail.com>
2025-09-30 22:41:14 -0500

    Se agrego la pantalla de recordatorios y la imagen que lo acompaña "recordatorio_superior.png"

--- app2/src/main/java/com/example/android_project/ui/pantallas/Recordatorios.kt
+++ app2/src/main/java/com/example/android_project/ui/pantallas/Recordatorios.kt
@@ -1,5 +1,6 @@
 package com.example.android_project.ui.pantallas
 
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
